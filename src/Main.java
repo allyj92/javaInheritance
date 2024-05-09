6b08d4914df96fd02504c6a386c64c86473f9ad6
eunjaelim <imeunjae361@gmail.com>
2024-05-09 10:53:59 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.go();
+
+        Bicycle bike = new Bicycle();
+        bike.stop();
+
+        System.out.println(car.doors);
+        System.out.println(bike.pedals);
+        }
+    }
